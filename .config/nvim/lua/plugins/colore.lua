765aa1e11e39ca0f1fd8c91435d73181545091af
Justin Albano <albano.justin@gmail.com>
2024-06-26 23:04:49 -0400

    Configured color scheme.

--- .config/nvim/lua/plugins/colore.lua
+++ .config/nvim/lua/plugins/colore.lua
@@ -1,10 +1,19 @@
 return {
-  { "rose-pine/neovim", name = "rose-pine" },
-  { "rebelot/kanagawa.nvim", name = "kanagawa" },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "rose-pine",
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        opts = {
+            disable_background = true,
+            styles = {
+                italic = false,
+            },
+        },
     },
-  },
+    { "rebelot/kanagawa.nvim", name = "kanagawa" },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "rose-pine-moon",
+        },
+    },
 }
