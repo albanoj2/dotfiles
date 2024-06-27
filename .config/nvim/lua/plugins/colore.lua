7311c6efb7add929288f5e0a44e02017f652a736
Justin Albano <albano.justin@gmail.com>
2024-06-26 22:44:31 -0400

    Initial commit

---
+++ .config/nvim/lua/plugins/colore.lua
@@ -0,0 +1,10 @@
+return {
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "rebelot/kanagawa.nvim", name = "kanagawa" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
