5648f072ce50a25d9bc9faa2b3d86697159a46c9
Justin Albano <albanoj2@users.noreply.github.com>
2024-07-07 13:47:40 -0400

    Added Oh My ZSH

--- .install-packages.sh
+++ .install-packages.sh
@@ -3,3 +3,5 @@
 CWD=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 sudo apt install $(cat $CWD/.package-list.txt)
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
